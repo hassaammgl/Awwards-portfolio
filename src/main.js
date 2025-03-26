4f57249c1ea4a75f581fff9c3635aae383017aa6
hassaammgl <h455a4mmehtab@gmail.com>
2025-03-27 02:35:49 +0500

    Add loading animation and update font styles in index.html, main.js, and style.css

--- src/main.js
+++ src/main.js
@@ -1,29 +1,65 @@
-import gsap from "gsap";
+import gsap, { Circ, Power3 } from "gsap";
 
-const h1Elems = document.getElementsByClassName("reveal");
+const revealToSpan = () => {
+    const h1Elems = document.getElementsByClassName("reveal");
 
-for (let i = 0; i < h1Elems.length; i++) {
-    const e = h1Elems[i];
+    for (let i = 0; i < h1Elems.length; i++) {
+        const e = h1Elems[i];
 
-    const orignalContent = e.innerHTML;
-    console.log(orignalContent);
+        const orignalContent = e.innerHTML;
+        console.log(orignalContent);
 
-    e.innerHTML = "";
+        e.innerHTML = "";
 
-    const spanP = document.createElement("span");
-    spanP.classList.add("parent")
-    const spanC = document.createElement("span");
-    spanC.classList.add("child")
+        const spanP = document.createElement("span");
+        spanP.classList.add("parent")
+        const spanC = document.createElement("span");
+        spanC.classList.add("child")
 
-    spanC.innerHTML = orignalContent;
+        spanC.innerHTML = orignalContent;
 
-    spanP.appendChild(spanC);
-    e.appendChild(spanP);
+        spanP.appendChild(spanC);
+        e.appendChild(spanP);
+    }
+
+    // gsap.to(".child", {
+    //     duration: 1,
+    //     transform: "translate(0, 0%)",
+    //     ease: "expo.inOut",
+    //     // stagger: .2
+    // })
+
 }
+revealToSpan()
 
-gsap.to(".child", {
-    duration: 1,
-    transform: "translate(0, 0%)",
-    ease: "expo.inOut",
-    stagger: .2
-})
+var tl = gsap.timeline();
+
+tl
+    .from('.child span', {
+        x: 100,
+        delay: 1,
+        stagger: .2,
+        duration: 1.4,
+        ease: Power3.easeInOut
+    })
+    .to('.parent .child', {
+        y: "-100%",
+        duration: 1,
+        ease: Circ.easeInOut
+    })
+    .to('#loader', {
+        height: 0,
+        duration: 1,
+        ease: Circ.easeInOut
+    })
+    .to('#green', {
+        height: "100%",
+        top: 0,
+        duration: -.8,
+        ease: Circ.easeInOut
+    })
+    .to('#green', {
+        height: "0%",
+        duration: -.8,
+        ease: Circ.easeInOut
+    })

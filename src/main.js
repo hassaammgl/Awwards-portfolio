4319a9be98e72c2a548c364627c24fe8a08e0589
hassaammgl <h455a4mmehtab@gmail.com>
2025-03-26 07:21:19 +0500

    testing for loading animation compleate

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import gsap from "gsap";
+
+const h1Elems = document.getElementsByClassName("reveal");
+
+for (let i = 0; i < h1Elems.length; i++) {
+    const e = h1Elems[i];
+
+    const orignalContent = e.innerHTML;
+    console.log(orignalContent);
+
+    e.innerHTML = "";
+
+    const spanP = document.createElement("span");
+    spanP.classList.add("parent")
+    const spanC = document.createElement("span");
+    spanC.classList.add("child")
+
+    spanC.innerHTML = orignalContent;
+
+    spanP.appendChild(spanC);
+    e.appendChild(spanP);
+}
+
+gsap.to(".child", {
+    duration: 1.5,
+    transform: "translate(0, 0%)",
+    ease: "expo.inOut"
+})
